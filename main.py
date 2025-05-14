17b64674ef218183ed74552835aab35feca667b8
damionmatthews <damionmatthews@gmail.com>
2025-05-13 23:24:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -53,8 +53,8 @@ async def login_to_projectx():
     async with httpx.AsyncClient() as client:
         try:
             response = await client.post(
-                "https://gateway-api-demo.s2f.projectx.com/api/Auth",
-                json={"key": TOPSTEP_API_KEY}
+                "https://api.topstepx.com/api/Auth/loginKey",
+                json={"userName": os.getenv("TOPSTEP_USERNAME"), "apiKey": TOPSTEP_API_KEY}
             )
             response.raise_for_status()
             SESSION_TOKEN = response.json().get("token")
