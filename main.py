0c3dcff374d29c4857d585fda4bb81f1578d68ca
damionmatthews <damionmatthews@gmail.com>
2025-05-16 00:24:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from signalRClient import setupSignalRConnection, closeSignalRConnection, get_ev
 import logging
 import os
 import asyncio
-from userHubClient import setupUserHubConnection, register_user_trade_callback
+from userHubClient import setupUserHubConnection
 
 app = FastAPI()
 
